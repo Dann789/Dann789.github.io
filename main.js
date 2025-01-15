e3213cbf60bfe91e9830c6c79daed804ddc4424c
Muhammad Wildan Ramadhana <wildan.rama2004@gmail.com>
2025-01-15 21:09:27 +0700

    add img + effect fade change img(home)

--- main.js
+++ main.js
@@ -3,4 +3,21 @@ var typed = new Typed(".auto-type", {
     typeSpeed: 150,
     backSpeed: 150,
     loop: true,
-  });
+});
+
+const image = document.getElementById("image");
+const listImage = ["images/profile2.jpg", "images/profile.jpg"];
+let index = 0;
+
+function changeImage() {
+    image.style.opacity = 0;
+
+    setTimeout(() => {
+        indexImage = index % listImage.length;
+        image.src = listImage[indexImage];
+        image.style.opacity = 1;
+        index++
+    }, 1000);
+}
+
+setInterval(changeImage, 5000);
