3f918ce2c95543f8b45e02def0bd27291a0d6e06
Muhammad Wildan Ramadhana <wildan.rama2004@gmail.com>
2025-04-11 22:47:52 +0700

    update nav & about me (not fix)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type", {
-    strings: ["Front End Developer", "Web Developer"],
+    strings: ["Web Developer", "Mobile Developer"],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true,
